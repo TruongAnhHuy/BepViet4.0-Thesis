fbf16ac716396643b5f220e1588180bf5718ed0c
Truong Anh Huy <0306231113@caothang.edu.vn>
2026-01-15 20:25:56 +0700

    Code ban dau

--- Laravel/laravel-bepviet4.0/routes/web.php
+++ Laravel/laravel-bepviet4.0/routes/web.php
@@ -5,27 +5,4 @@ use App\Http\Controllers\AuthController;
 Route::get('/', function () {
     return view('welcome');
 });
-// Route hiển thị form
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
-
-// Route xử lý dữ liệu gửi lên (Post)
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
-
-Route::prefix('admin')->name('admin.')->group(function () {
-    
-    Route::get('/dashboard', function () {
-        return view('admin.dashboard');
-    })->name('dashboard');
-
-    Route::get('/userreport', function () {
-        return view('admin.UserReport');
-    })->name('userreport');
-    Route::get('/users', function () {
-        return view('admin.Users');
-    })->name('users');
-    // Sau này thêm các route khác:
-    // Route::get('/users', [UserController::class, 'index'])->name('users');
-});
 

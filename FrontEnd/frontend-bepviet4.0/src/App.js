70dc805fc2a0ed34cd580419fbef3d85801711fb
MaiTruongDu <0306231097@caothang.edu.vn>
2026-01-16 22:27:05 +0700

    vưa sua file app.js

--- FrontEnd/frontend-bepviet4.0/src/App.js
+++ FrontEnd/frontend-bepviet4.0/src/App.js
@@ -12,21 +12,16 @@ function App() {
   return (
     <Router>
       <div className="app-container">
-        {/* Sidebar nằm cố định ở đây */}
         <Sidebar />
 
-        {/* Phần bên phải sẽ thay đổi tùy theo Link */}
         <div className="content-area" style={{ flex: 1, marginLeft: '260px' }}>
           <Routes>
-            {/* Mặc định vào trang quản lý bài viết hoặc trang chủ tùy bạn */}
-            
             
             <Route path="/posts" element={<PostManagement />} />
             <Route path="/recipes" element={<RecipeManagement />} />
             <Route path="/reports" element={<QuanLyToCao />} />
             <Route path="/users" element={<QuanLyUser />} />
-            {/* Thêm các Route khác sau này */}
-            
+
           </Routes>
         </div>
       </div>

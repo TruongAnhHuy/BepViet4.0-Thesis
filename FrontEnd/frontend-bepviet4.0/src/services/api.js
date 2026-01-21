b6e6488eb83c51a5bb546ebfbb4a12a20b048bd6
MaiTruongDu <0306231097@caothang.edu.vn>
2026-01-21 20:12:23 +0700

    vừa cập nhập file api.js

--- FrontEnd/frontend-bepviet4.0/src/services/api.js
+++ FrontEnd/frontend-bepviet4.0/src/services/api.js
@@ -24,3 +24,30 @@ export async function getBlogs() {
   if (!res.ok) throw new Error("Failed to fetch blogs");
   return res.json();
 }
+
+//lấy ds bài viết
+export const getPosts = () => {
+    return axiosClient.get('/posts');
+};
+
+// export const updatePostStatus = (id, status) => {
+//     // Gửi body là { status: 1 } hoặc { status: 0 }
+//     return axiosClient.put(`/posts/${id}/status`, { status: status });
+// };
+
+export const updatePostStatus = async (id, newStatus) => {  //cập nhật trạng thái bài viết
+    // Ví dụ: Backend cần số 1 hoặc 0
+    const statusValue = newStatus === "Chờ duyệt" ? 1 : 0; 
+
+    // Gọi API (Đường dẫn này phải khớp với route Laravel của bạn)
+    const response = await fetch(`http://127.0.0.1:8000/api/posts/${id}/status`, {
+        method: 'PUT', // Hoặc POST tùy backend
+        headers: {
+            'Content-Type': 'application/json',
+            // 'Authorization': 'Bearer ...' // Nếu có token
+        },
+        body: JSON.stringify({ status: statusValue })
+    });
+
+    return response.json();
+};

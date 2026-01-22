b49d9c15a49f841aa3e66a97e5777f1f1da72d9e
MaiTruongDu <0306231097@caothang.edu.vn>
2026-01-22 14:19:01 +0700

    vừa cập nhập thêm route

--- Laravel/laravel-bepviet4.0/routes/api.php
+++ Laravel/laravel-bepviet4.0/routes/api.php
@@ -18,3 +18,8 @@ Route::get('/blogs', [BlogController::class, 'index']);
 Route::get('/recipes', function () {
     return Recipe::all();
 });
+
+//trang quan ly bài viết
+Route::get('/posts', [PostController::class, 'index']);
+// Route cập nhật trạng thái
+Route::put('/posts/{id}/status', [PostController::class, 'updateStatus']);
